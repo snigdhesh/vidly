02f5ce632d3a53625d558b871f96b927dc2da1de
snigdhesh <snigdhesh@live.com>
2024-04-24 10:49:44 -0400

    Publish static content

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express') //This returns a function
+
 const app = express() //This returns an object
 
-app.get('/', (req, res) => {res.send("<h1>Hello world</h1>") })
+app.use(express.static('./public')) //This is a middleware that serves static files from the public directory
+
+app.get('/', (req, res) => {res.send() })
 
 app.listen(3000, () => {console.log("listening on port 3000..")} )
