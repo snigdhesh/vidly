56092098e3d5d9e9762604983289525af1a400fa
snigdhesh <snigdhesh@live.com>
2024-04-24 11:09:36 -0400

    Set PORT to environment variable

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json()) //This is a middleware that parses incoming requests wit
 app.use('/api/genres', genres) //This is a middleware that uses the genres route
 app.use('/',home) //This is a middleware that uses the home route
 
+const port = process.env.PORT || 3000 
+//You can set env variable on WINDOWS, like shown below
+//set PORT=5000
 
-
-app.listen(3000, () => {console.log("listening on port 3000..")} )
+app.listen(port, () => {console.log(`listening to port ${port}`)} )
