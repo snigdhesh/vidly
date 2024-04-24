7222ee094d1fb4ccc3aa55fbb9f02c0b70107b0f
snigdhesh <snigdhesh@live.com>
2024-04-24 11:04:13 -0400

    Organize home route

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express') //This returns a function
 const genres = require('./routes/genres') //This returns an object
+const home = require('./routes/home') //This returns an object
 const app = express() //This returns an object
 
 app.use(express.static('./public')) //This is a middleware that serves static files from the public directory
 app.use(express.json()) //This is a middleware that parses incoming requests with JSON payloads and is based on body-parser
 
 app.use('/api/genres', genres) //This is a middleware that uses the genres route
+app.use('/',home) //This is a middleware that uses the home route
 
-app.get('/', (req, res) => {res.send() })
+
 
 app.listen(3000, () => {console.log("listening on port 3000..")} )
