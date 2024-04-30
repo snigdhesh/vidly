984501fcdab0b418acef177189249ef02f2ff84b
snigdhesh <snigdhesh@live.com>
2024-04-30 07:50:20 -0400

    Add Persistence to Genres API

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const genres = require('./routes/genres') //This returns an object
 const home = require('./routes/home') //This returns an object
 const app = express() //This returns an object
 const auth = require('./middleware/auth') //This returns a function
+const database = require('mongoose') //This returns an object
+const config = require('config') //This returns an object
+
+//connect to database when application starts
+database.connect(config.get('datasource.url'))
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err))
 
 app.use(express.static('./public')) //This is a middleware that serves static files from the public directory
 app.use(express.json()) //This is a middleware that parses incoming requests with JSON payloads and is based on body-parser
 app.use(auth) //This is example of a custom middleware
 app.use('/api/genres', genres) //This is a middleware that uses the genres route
-app.use('/',home) //This is a middleware that uses the home route
+app.use('/', home) //This is a middleware that uses the home route
 
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 //You can set env variable on WINDOWS, like shown below
 //set PORT=5000
 
-app.listen(port, () => {console.log(`listening to port ${port}`)})
+app.listen(port, () => { console.log(`listening to port ${port}`) })
--- routes/genres.js
+++ routes/genres.js
@@ -1,60 +1,75 @@
 const express = require('express')
 const Joi = require('joi') //This returns a class
 const router = express.Router();
+const mongoose = require('mongoose') //This returns an object
 
-const genres = [
-    { id: 1, name: 'Action', description: 'Movies with lots of action and excitement.' },
-    { id: 2, name: 'Comedy', description: 'Movies that make you laugh and smile.' },
-    { id: 3, name: 'Drama', description: 'Movies that evoke strong emotions and tell compelling stories.' },
-    { id: 4, name: 'Thriller', description: 'Movies that keep you on the edge of your seat with suspense.' },
-    { id: 5, name: 'Sci-Fi', description: 'Movies that explore futuristic concepts and scientific possibilities.' }
-];
+//Create schema
+const genreSchema = mongoose.Schema({
+    name: { type: String, required: true, minLength: 3, maxLength: 50 }
+})
+
+//Create class, using schema
+const Genre = mongoose.model('Genre', genreSchema)
+
 
 //Get list of genre objects
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
     res.send(genres)
- })
+})
 
- //Delete a genre object
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.')
-    const index = genres.indexOf(genre)
-    genres.splice(index, 1)
+
+//Add a genre object
+router.post('/', async (req, res) => {
+    const { error } = validateGenre(req.body) //This returns an object
+    if (error) return res.status(400).send(error.details[0].message)
+
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save()
     res.send(genre)
- })
+})
 
-  //Add a genre object
-router.post('/', (req, res) => {
-    const result = validateGenre(req.body);
-    
-    if(result.error) return res.status(400).send(result.error.details[0].message)
 
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name,
-        description: req.body.description
+//Update a genre object
+router.put('/:id', async (req, res) => {
+    const { error } = validateGenre(req.body) //This returns an object
+    if (error) return res.status(400).send(error.details[0].message)
+    let genre;
+    try {
+        genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    } catch (ex) {
+        console.log(ex.message)
     }
-    genres.push(genre)
-    res.send(genre)
- })
-
-   //Update a genre object
-router.put('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id))
     if (!genre) return res.status(404).send('The genre with the given ID was not found.')
-    genre.name = req.body.name
-    genre.description = req.body.description
-    res.send(genre)
- })
 
- const validateGenre = (genre) => {
+    res.send(genre)
+})
+
+//Delete a genre object
+router.delete('/:id', async (req, res) => {
+
+    const genre = await Genre.findByIdAndDelete(req.params.id)
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.')
+
+    res.send(genre)
+})
+
+
+//Get single genre object
+router.get('/:id', async (req, res) => {
+    const genres = await Genre.findById(req.params.id);
+    res.send(genres)
+})
+
+
+
+const validateGenre = (genre) => {
     const schema = Joi.object({
         name: Joi.string().min(3).required(),
-        description: Joi.string().min(3).required()
+        //description: Joi.string().min(3).required()
     })
-    
-    return schema.validate(genre) //This returns an object
- }
 
- module.exports = router;
+    return schema.validate(genre) //This returns an object
+}
+
+module.exports = router;
