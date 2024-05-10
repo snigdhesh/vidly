39882fa45bcb7dc49985c19db473f0be576f627d
snigdhesh <snigdhesh@live.com>
2024-05-10 08:02:08 -0400

    Externalize routes from index.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 require('express-async-errors') //We need to load this module, when application starts. So this should be declared first.
 const express = require('express') //This returns a function
-const app = express() //This returns an object
+const app = express() //This returns an object. This should be a single instance for entire application
 
 const database = require('mongoose') //This returns an object
 const config = require('config') //This returns an object
 
 //Routes
-const genres = require('./routes/genres') //This returns an object
-const home = require('./routes/home') //This returns an object
-const customers = require('./routes/customers') //This returns an object
-const movies = require('./routes/movies') //This returns an object
-const rentals = require('./routes/rentals') //This returns an object
-const users = require('./routes/users') //This returns an object
-const auth = require('./routes/auth') //This returns an object
-
-const globalErrorHandler = require('./middleware/error') //This returns a function
+require('./startup/routes')(app)
 
 //Import winston module, to log errors to a file/console.
 const winston = require('winston')
@@ -33,8 +25,7 @@ winston.add(new winston.transports.MongoDB({
 //We can use new log file, to log all uncaught exceptions. This will log all uncaught exceptions to a file called 'uncaughtExceptions.log' 
 winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExceptions.log'}))
 
- //This error is thrown on purpose to test uncaught exception handling piece of code we wrote above.
-throw new Error('Something failed during startup') //This is an uncaught exception. This will be caught by the global exception handler.
+
 
 if(!config.get('privateKey')) { //This is a private key that is used to sign the token. This is stored in the config file
     console.error('FATAL ERROR: Private key is not set');
@@ -48,19 +39,7 @@ database.connect(config.get('datasource.url'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
 
-app.use(express.static('./public')) //This is a middleware that serves static files from the public directory
-app.use(express.json()) //This is a middleware that parses incoming requests with JSON payloads and is based on body-parser
 
-app.use('/api/genres', genres) //This is a middleware that uses the genres route
-app.use('/api/customers', customers) //This is a middleware that uses the customers route
-app.use('/api/movies', movies) //This is a middleware that uses the movies route
-app.use('/api/rentals', rentals) //This is a middleware that uses the rentals route
-app.use('/api/users', users) //This is a middleware that uses the users route
-app.use('/api/auth', auth) //This is a middleware that uses the auth route
-app.use('/', home) //This is a middleware that uses the home route
-
-//Global exception handling should be done after all middleware and routes are set up.
-app.use(globalErrorHandler) //globalErrorHandler will catch any error that is thrown before this line.
 
 
 const port = process.env.PORT || 3000
---
+++ startup/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const genres = require('../routes/genres') //This returns an object
+const home = require('../routes/home') //This returns an object
+const customers = require('../routes/customers') //This returns an object
+const movies = require('../routes/movies') //This returns an object
+const rentals = require('../routes/rentals') //This returns an object
+const users = require('../routes/users') //This returns an object
+const auth = require('../routes/auth') //This returns an object
+const globalErrorHandler = require('../middleware/error') //This returns a function
+
+const routes = (app) => {
+    app.use(express.static('../public')) //This is a middleware that serves static files from the public directory
+    app.use(express.json()) //This is a middleware that parses incoming requests with JSON payloads and is based on body-parser
+    //Routes
+    app.use('/api/genres', genres) //This is a middleware that uses the genres route
+    app.use('/api/customers', customers) //This is a middleware that uses the customers route
+    app.use('/api/movies', movies) //This is a middleware that uses the movies route
+    app.use('/api/rentals', rentals) //This is a middleware that uses the rentals route
+    app.use('/api/users', users) //This is a middleware that uses the users route
+    app.use('/api/auth', auth) //This is a middleware that uses the auth route
+    app.use('/', home) //This is a middleware that uses the home route
+    
+    //Global exception handling should be done after all middleware and routes are set up.
+    app.use(globalErrorHandler) //globalErrorHandler will catch any error that is thrown before this line.
+}
+
+module.exports = routes;
