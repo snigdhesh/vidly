fd6bb78fde15d58653be35019757bace9d094349
snigdhesh <snigdhesh@live.com>
2024-05-12 10:43:45 -0400

    Integration Test: Test /api/genres response using 'supertest' library

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,6 @@
 const request = require('supertest') //This module returns a function
+const {Genre, validate} = require('../../models/genre');
+const winston = require('winston')
 //load server from index.js
 let server;
 
@@ -7,15 +9,31 @@ describe('/api/genres',()=> {
     
     //Note: Load server before test executions and close it after test executions
     beforeEach(()=>{ server = require('../../index') })
-    beforeEach(()=>{ server.close() })
+    afterEach(async()=>{ 
+        server.close() ;
+        winston.info('Server closed')
+        await Genre.deleteMany({});
+    })
 
     //sub test suite
     describe('/GET',()=>{
 
         //Test case 1
         it('should return all genres', async () => {
+        
+            //With this command, we can add multiple documents to mongoDB at once.
+            //But this will keep adding these documents, whenever test case runs, that happens every time we make a change, due to 'jest --watchAll'
+            //So cleanup this collection in afterEach() method
+            await Genre.collection.insertMany([
+                { name: 'genre1'},
+                { name: 'genre2'}
+            ]);
+
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(genre => genre.name == 'genre1')).toBeTruthy(); //Every array has 'some()' method
+            expect(res.body.some(genre => genre.name == 'genre2')).toBeTruthy(); //Every array has 'some()' method
         })
     })
 
